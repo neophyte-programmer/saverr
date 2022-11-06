43e8fb6727f893deb064c83082a693bfc6d0c03a
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-11-06 13:26:47 +0000

    feat: :sparkles: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { useStateValue } from '../context/StateProvider'
 const Footer = () => {
     const [{ theme }] = useStateValue()
   return (
-      <footer className={`absolute bottom-0 max-w-screen w-full flex items-center justify-center py-3 border-t text-sm 
+      <footer className={`absolute bottom-0 max-w-[85vw] mx-auto w-full flex items-center justify-center py-3 border-t text-sm 
       ${ theme === 'dark' ? 'border-gray-700' : 'border-gray-200'} `}>
           <p> <span className='text-green-500 text-semibold'>Saverr</span> by <a href="https://github.com/neophyte-programmer" target='_blank' className='border-b border-green-200'>Nutifafa</a> </p>
     </footer>
