2d53d92d814f66d1febc935f8137e5f1799cee57
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-11-06 13:30:12 +0000

    feat: :sparkles: updated types

--- utils/types.tsx
+++ utils/types.tsx
@@ -1,3 +1,5 @@
+import { StaticImageData } from "next/image"
+
 export type Button = {
     text: string
     onClick?: () => void
@@ -12,4 +14,37 @@ export type NavItem = {
     path?: string
     icon: JSX.Element
     onClick?: () => void
+}
+
+export type HeaderProps = {
+    title?: string
+}
+
+export type InfoCard = {
+    text: string
+    path: string
+    isAlt: boolean
+    image: string | StaticImageData
+    isExternal?: boolean
+}
+
+export type DashCard = {
+    icon: JSX.Element
+    text: string
+    value: number
+    isAlt?: boolean
+}
+
+export type ProgressCircle = {
+    color: string
+    percentage: number | 0
+    radius: number
+    
+}
+export type ProgressText = {
+    percentage: number
+}
+export type ProgressPie = {
+    radius: number
+    percentage: number
 }
